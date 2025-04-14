0a9687d7cc42226cda307c4c2efb7da2a4fe282e
Rafael Joseph Zaide <rafaeljosephzaide06@gmail.com>
2025-04-14 14:06:30 +0800

    fix: double exports of VetService in schema.ts

--- resources/js/types/schema.ts
+++ resources/js/types/schema.ts
@@ -33,14 +33,6 @@ export interface VetService {
     description?: string;
     created_at: string;
     updated_at: string;
-}
-
-export interface VetService {
-    id: number;
-    name: string;
-    description?: string;
-    created_at: string;
-    updated_at: string;
     types: VetServiceType[];
 }
 
