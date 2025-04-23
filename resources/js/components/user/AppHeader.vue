dda44075eec44ef52fbbd5e17159b4ac5bf8e870
unknown <sevillajohnmichael>
2025-04-23 15:32:37 +0800

    feat: imports of icon in AppHeader

--- resources/js/components/user/AppHeader.vue
+++ resources/js/components/user/AppHeader.vue
@@ -18,7 +18,7 @@ import UserMenuContent from '@/components/UserMenuContent.vue';
 import { getInitials } from '@/composables/useInitials';
 import type { BreadcrumbItem, NavItem } from '@/types';
 import { Link, usePage } from '@inertiajs/vue3';
-import { BriefcaseMedical, LayoutGrid, Home, Menu, LogIn, UserPlus, Info, Calendar1 } from 'lucide-vue-next';
+import { BriefcaseMedical, HandHelping, Home, Menu, LogIn, UserPlus, Info, Calendar1, ShoppingBasket } from 'lucide-vue-next';
 import { computed, ref } from 'vue';
 
 interface Props {
